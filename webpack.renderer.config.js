a7fc7343f15d3142ce553f91ef81fd179a285d32
Alfonso Gomez <ago@topfloormarketing.net>
2019-10-15 11:14:43 -0600

    added new rule to use the babel loader

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -1,19 +1,19 @@
-const rules = require('./webpack.rules');
+const rules = require('./webpack.rules')
 
 rules.push({
   test: /\.css$/,
-  use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
-});
+  use: [{ loader: 'style-loader' }, { loader: 'css-loader' }]
+})
 
 rules.push({
-    test: /\.jsx?$/,
-    exclude: /node_modules/,
-    use: [{ loader: 'babel-loader' }]
+  test: /\.jsx?$/,
+  exclude: /node_modules/,
+  use: [{ loader: 'babel-loader' }]
 })
 
 module.exports = {
   // Put your normal webpack config below here
   module: {
-    rules,
-  },
-};
+    rules
+  }
+}
