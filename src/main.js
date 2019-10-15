699a35a61bb637829edf66faeb45690607d82b5d
Gustavo Gomez <gustavogomez092@gmail.com>
2019-10-15 14:52:04 -0600

    Changed With of the window

--- src/main.js
+++ src/main.js
@@ -12,7 +12,7 @@ let mainWindow
 const createWindow = () => {
   // Create the browser window.
   mainWindow = new BrowserWindow({
-    width: 800,
+    width: 1000,
     height: 600,
     show: false
   })
