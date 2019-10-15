1e5ea8bef2db1f3c9c1f0f6299f3a707ddee54a7
Alfonso Gomez <ago@topfloormarketing.net>
2019-10-15 11:15:23 -0600

    added lazy imports and created routing basic setup

--- src/renderer.js
+++ src/renderer.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { Suspense, lazy } from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+import { MemoryRouter as Router, Route, Switch } from 'react-router-dom'
 
-console.log('👋 This message is being logged by "renderer.js", included via webpack');
+// Lazy imports
+const Setup = lazy(() => import('./views/setup'))
 
 function App () {
-  return <div>1234</div>
+  return (
+    <Router>
+      <Switch>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Route path='/' exact component={Setup} />
+        </Suspense>
+      </Switch>
+    </Router>
+  )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'))
