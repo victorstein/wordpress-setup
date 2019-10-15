d3f1cced3a874f03b22a85e2f6f45852b2a99399
Gustavo Gomez <gustavogomez092@gmail.com>
2019-10-15 16:52:24 -0600

    boostrapped the domain view

--- src/views/Wizard/DomainSetUp.js
+++ src/views/Wizard/DomainSetUp.js
@@ -1,18 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Row, Col, Button, Input, Alert } from 'reactstrap'
+import { useInput, addEntries } from '../../utils'
 
 const Domain = (props) => {
+  const [alert, setAlert] = useState({
+    msg: null,
+    color: null
+  })
+  const [inputs, setInputs] = useInput()
+
+  console.log(inputs)
+
+  const addDomain = async () => {
+    try {
+      await addEntries(inputs)
+      setAlert({ msg: 'Paths to new domain added successfully', color: 'success' })
+      setTimeout(_ => props.nextStep(), 1000)
+    } catch (e) {
+      console.log(e)
+      setAlert({ msg: e.message, color: 'danger' })
+    }
+  }
+
   return (
     <Row className='align-items-center'>
       <Col>
-        <Input id='domain' type='text' className='w-100 mb-1' placeholder='Add domain' />
-        <Input id='suffix' type='text' className='w-100 mb-3' placeholder='Add domain suffix' />
+        <Input name='domain' onChange={setInputs} type='text' className='w-100 mb-1' placeholder='Add domain' />
+        <Input name='suffix' onChange={setInputs} type='text' className='w-100 mb-3' placeholder='Add domain suffix' />
         <div className='d-flex flex-row'>
-          <Button color='success' onClick={props.nextStep} block>ADD SITE</Button>
+          <Button color='success' onClick={addDomain} block>ADD SITE</Button>
         </div>
-        <Alert className='mt-4' color='primary'>
-          It worked!
-        </Alert>
+        {
+          alert.msg
+            ? <Alert className='mt-4' color={alert.color}>
+              { alert.msg }
+            </Alert>
+            : null
+        }
       </Col>
       <Col>
         <h1 className='font-weight-bold'>DOMAIN INFO</h1>
