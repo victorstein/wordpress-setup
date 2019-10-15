388d9f801551fd80569fadb5dc90957f63714d51
Gustavo Gomez <gustavogomez092@gmail.com>
2019-10-15 14:52:32 -0600

    Welcome screen

--- src/views/Wizard/Welcome.js
+++ src/views/Wizard/Welcome.js
@@ -1,14 +1,26 @@
 import React from 'react'
-import { Row, Col, Button } from 'reactstrap'
+import { Row, Col, Button, Input, Alert } from 'reactstrap'
 
 const Welcome = () => {
   return (
-    <Row>
+    <Row className='align-items-center'>
       <Col>
         <img src={require('../../assets/img/wp.png')} className='img-fluid' alt='importer' />
       </Col>
       <Col>
-        <h1>welcome</h1>
+        <h1 className='font-weight-bold'>WELCOME</h1>
+        <p>Welcome to the WordPress enviroment setup wizzard!</p>
+        <p className='mb-3'>This wizzard will guide you through all the steps necessary to get up and running with your WordPress
+          development environment. Make sure you have XAMPP installed before you proceed, To get started please add the
+          path to your XAMPP installation:
+        </p>
+        <div className='d-flex flex-row'>
+          <Input type='text' className='w-50 mr-4' placeholder='C:\xampp' />
+          <Button color='success'>CHECK NOW</Button>
+        </div>
+        <Alert className='mt-4' color='primary'>
+          It worked!
+        </Alert>
       </Col>
     </Row>
   )
