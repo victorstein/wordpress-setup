20c6ae810f40413ba965583c532c09287266c507
Gustavo Gomez <gustavogomez092@gmail.com>
2019-10-17 18:30:56 -0600

    added public path to webpack config

--- webpack.main.config.js
+++ webpack.main.config.js
@@ -4,6 +4,9 @@ module.exports = {
    * that runs in the main process.
    */
   entry: './src/main.js',
+  output: {
+    publicPath: ''
+  },
   // Put your normal webpack config below here
   module: {
     rules: require('./webpack.rules')
